a1d81fe815d33fdeb92856bb6c78df3c900b1076
Eddie-Anguiano <eddiesanguiano@gmail.com>
2018-12-17 13:28:11 -0800

    define an appState and set it to false so buttons do not continue to work after the app has already run

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /*eslint-env browser*/
-var numServers,
+var appState = true,
+	numServers,
 	sales,
 	ccTips,
 	cashTips,
@@ -249,125 +250,122 @@ toggleButton.addEventListener("click", function () {
 	clearDefaultSpecific();
 })
 addButton.addEventListener("click", function () {
-	var employeeName = document.getElementById("employeeName").value;
-	var hours = parseFloat(document.getElementById("hours").value);
-	var mins = parseFloat(document.getElementById("mins").value);
-	var employeeTime = convertTimeToDecimal(hours, mins);
+	if (appState) {
+		var employeeName = document.getElementById("employeeName").value;
+		var hours = parseFloat(document.getElementById("hours").value);
+		var mins = parseFloat(document.getElementById("mins").value);
+		var employeeTime = convertTimeToDecimal(hours, mins);
 
 
-	function addHours(array) {
-		totalHours = 0;
-		for (var i = 0; i < array.length; i++) {
-			totalHours += array[i].time;
+		function addHours(array) {
+			totalHours = 0;
+			for (var i = 0; i < array.length; i++) {
+				totalHours += array[i].time;
+			}
 		}
+
+		function displayHourlyStats() {
+			var displayStats = document.querySelector(".hourlyStats");
+
+			displayStats.textContent = " \xa0\xa0\xa0\xa0" + hourlyServerCount + " Servers \xa0\xa0\xa0\xa0Total Hours: " + totalHours.toFixed(2);
+		}
+
+		function displayServerList() {
+			var ul = document.querySelector(".serverList");
+			var li = document.createElement("li");
+
+			li.className = "serverListLI";
+			li.textContent = "Name: " + allData[hourlyServerCount - 1].name + " \xa0\xa0Hours: " + allData[hourlyServerCount - 1].time.toFixed(2);
+			ul.appendChild(li);
+
+		}
+
+		allData.push({
+			name: employeeName,
+			time: employeeTime
+		})
+		hourlyServerCount += 1;
+		addHours(allData);
+		displayHourlyStats();
+		displayServerList();
+		//console.table(allData);
 	}
-
-	function displayHourlyStats() {
-		var displayStats = document.querySelector(".hourlyStats");
-
-		displayStats.textContent = " \xa0\xa0\xa0\xa0" + hourlyServerCount + " Servers \xa0\xa0\xa0\xa0Total Hours: " + totalHours.toFixed(2);
-	}
-
-	function displayServerList() {
-		var ul = document.querySelector(".serverList");
-		var li = document.createElement("li");
-
-		li.className = "serverListLI";
-		li.textContent = "Name: " + allData[hourlyServerCount - 1].name + " \xa0\xa0Hours: " + allData[hourlyServerCount - 1].time.toFixed(2);
-		ul.appendChild(li);
-
-	}
-
-	allData.push({
-		name: employeeName,
-		time: employeeTime
-	})
-	hourlyServerCount += 1;
-	addHours(allData);
-	displayHourlyStats();
-	displayServerList();
-	//console.table(allData);
 })
 submitHourlybutton.addEventListener("click", function () {
-	var byHourlyEmployeeSales = "byHourlyEmployeeSales";
-	var byHourlyEmployeeTips = "byHourlyEmployeeTips";
-	var byHourlyEmployeeBusTips = "byHourlyEmployeeBusTips";
-	var byHourlyEmployeeBarTips = "byHourlyEmployeeBarTips";
-	var byHourlyEmployeeExpoTips = "byHourlyEmployeeExpoTips";
-	var byHourlyEmployeeTotalTipOut = "byHourlyEmployeeTotalTipOut";
-	var byHourlyEmployeeCashOwed = "byHourlyEmployeeCashOwed";
-	var byHourlyEmployeeClaimedTips = "byHourlyEmployeeClaimedTips";
+	if (appState) {
+		var byHourlyEmployeeSales = "byHourlyEmployeeSales";
+		var byHourlyEmployeeTips = "byHourlyEmployeeTips";
+		var byHourlyEmployeeBusTips = "byHourlyEmployeeBusTips";
+		var byHourlyEmployeeBarTips = "byHourlyEmployeeBarTips";
+		var byHourlyEmployeeExpoTips = "byHourlyEmployeeExpoTips";
+		var byHourlyEmployeeTotalTipOut = "byHourlyEmployeeTotalTipOut";
+		var byHourlyEmployeeCashOwed = "byHourlyEmployeeCashOwed";
+		var byHourlyEmployeeClaimedTips = "byHourlyEmployeeClaimedTips";
 
-	function calculateTotals() {
-		totalTips = ccTips + cashTips;
-		busTips = round(mdrTips * 0.1);
-		barTips = round(totalTips * 0.05);
-		totalExpoTips = totalTips * 0.05;
-		totalTipOut = totalExpoTips + busTips + barTips;
-		walkTips = totalTips - totalTipOut;
-	}
-
-	function calculatePerHour(totalVariable, newObjKey) {
-		var perHourVariable = totalVariable / totalHours;
-
-		for (var i = 0; i < allData.length; i++) {
-			allData[i][newObjKey] = perHourVariable * allData[i].time;
+		function calculateTotals() {
+			totalTips = ccTips + cashTips;
+			busTips = round(mdrTips * 0.1);
+			barTips = round(totalTips * 0.05);
+			totalExpoTips = totalTips * 0.05;
+			totalTipOut = totalExpoTips + busTips + barTips;
+			walkTips = totalTips - totalTipOut;
 		}
-	}
 
-	function calculateAllPerHours() {
-		calculatePerHour(sales, byHourlyEmployeeSales);
-		calculatePerHour(totalTips, byHourlyEmployeeTips);
-		calculatePerHour(busTips, byHourlyEmployeeBusTips);
-		calculatePerHour(barTips, byHourlyEmployeeBarTips);
-		calculatePerHour(totalExpoTips, byHourlyEmployeeExpoTips);
-		calculatePerHour(totalTipOut, byHourlyEmployeeTotalTipOut);
-		calculatePerHour(cashTips, byHourlyEmployeeCashOwed);
-		calculatePerHour(walkTips, byHourlyEmployeeClaimedTips);
-	}
+		function calculatePerHour(totalVariable, newObjKey) {
+			var perHourVariable = totalVariable / totalHours;
 
-	function displayAllData() {
-		for (var i = 0; i < allData.length; i++) {
-			var ouputContainer = document.getElementsByClassName("outputContainer")[0];
-			var allDataDiv = document.createElement("div");
-			var nameDiv = document.createElement("div");
-			var hourlyNameIcon = document.createElement("img");
-			var tableDiv = document.createElement("div");
-			var nameOutputDiv = document.createElement("div");
-
-			nameOutputDiv.className = "nameOutputDiv";
-			tableDiv.className = "tableDiv";
-			allDataDiv.className = "allDataDiv";
-			nameDiv.className = "nameDiv";
-			hourlyNameIcon.className = "hourlyNameIcon";
-			hourlyNameIcon.src = "Capture2.PNG";
-
-
-
-
-			nameOutputDiv.innerHTML = allData[i].name;
-			tableDiv.innerHTML = "<table><tr><td>Sales:\xa0\xa0$" + allData[i].byHourlyEmployeeSales.toFixed(2) + "</td><td>Busser Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeBusTips.toFixed(2) + "</td></tr><tr><td>Tips (Before Tip Out):\xa0\xa0$" + allData[i].byHourlyEmployeeTips.toFixed(2) + "</td><td>Bar Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeBarTips.toFixed(2) + "</td></tr><tr><td>Cash Owed:\xa0\xa0$" + allData[i].byHourlyEmployeeCashOwed.toFixed(2) + "</td><td>Runners/Expo(s) Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeExpoTips.toFixed(2) + "</td></tr><tr><td>Claimed Tips:\xa0\xa0$" + allData[i].byHourlyEmployeeClaimedTips.toFixed(2) + "</td><td>Total Tipout:\xa0\xa0$" + allData[i].byHourlyEmployeeTotalTipOut.toFixed(2) + "</td></tr></table>";
-			//			tableDiv.innerHTML += "<tr><td>3</td><td>4</td></tr>";
-			//			tableDiv.innerHTML += "<tr><td>5</td><td>6</td></tr>";
-			//			tableDiv.innerHTML += "<tr><td>7</td><td>8</td></tr></table";
-
-
-
-			ouputContainer.appendChild(allDataDiv);
-			allDataDiv.appendChild(tableDiv);
-			allDataDiv.insertBefore(nameDiv, tableDiv);
-			nameDiv.appendChild(nameOutputDiv);
-			nameDiv.insertBefore(hourlyNameIcon, nameOutputDiv);
+			for (var i = 0; i < allData.length; i++) {
+				allData[i][newObjKey] = perHourVariable * allData[i].time;
+			}
 		}
-	}
 
-	getActiveInputs();
-	validateInputArray();
-	convertInputToFloat();
-	calculateTotals();
-	calculateAllPerHours();
-	displayAllData();
-	console.table(allData);
+		function calculateAllPerHours() {
+			calculatePerHour(sales, byHourlyEmployeeSales);
+			calculatePerHour(totalTips, byHourlyEmployeeTips);
+			calculatePerHour(busTips, byHourlyEmployeeBusTips);
+			calculatePerHour(barTips, byHourlyEmployeeBarTips);
+			calculatePerHour(totalExpoTips, byHourlyEmployeeExpoTips);
+			calculatePerHour(totalTipOut, byHourlyEmployeeTotalTipOut);
+			calculatePerHour(cashTips, byHourlyEmployeeCashOwed);
+			calculatePerHour(walkTips, byHourlyEmployeeClaimedTips);
+		}
+
+		function displayAllData() {
+			for (var i = 0; i < allData.length; i++) {
+				var ouputContainer = document.getElementsByClassName("outputContainer")[0];
+				var allDataDiv = document.createElement("div");
+				var nameDiv = document.createElement("div");
+				var hourlyNameIcon = document.createElement("img");
+				var tableDiv = document.createElement("div");
+				var nameOutputDiv = document.createElement("div");
+
+				nameOutputDiv.className = "nameOutputDiv";
+				tableDiv.className = "tableDiv";
+				allDataDiv.className = "allDataDiv";
+				nameDiv.className = "nameDiv";
+				hourlyNameIcon.className = "hourlyNameIcon";
+				hourlyNameIcon.src = "Capture2.PNG";
+
+				nameOutputDiv.innerHTML = allData[i].name;
+				tableDiv.innerHTML = "<table><tr><td>Sales:\xa0\xa0$" + allData[i].byHourlyEmployeeSales.toFixed(2) + "</td><td>Busser Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeBusTips.toFixed(2) + "</td></tr><tr><td>Tips (Before Tip Out):\xa0\xa0$" + allData[i].byHourlyEmployeeTips.toFixed(2) + "</td><td>Bar Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeBarTips.toFixed(2) + "</td></tr><tr><td>Cash Owed:\xa0\xa0$" + allData[i].byHourlyEmployeeCashOwed.toFixed(2) + "</td><td>Runners/Expo(s) Tipped:\xa0\xa0$" + allData[i].byHourlyEmployeeExpoTips.toFixed(2) + "</td></tr><tr><td>Claimed Tips:\xa0\xa0$" + allData[i].byHourlyEmployeeClaimedTips.toFixed(2) + "</td><td>Total Tipout:\xa0\xa0$" + allData[i].byHourlyEmployeeTotalTipOut.toFixed(2) + "</td></tr></table>";
+
+				ouputContainer.appendChild(allDataDiv);
+				allDataDiv.appendChild(tableDiv);
+				allDataDiv.insertBefore(nameDiv, tableDiv);
+				nameDiv.appendChild(nameOutputDiv);
+				nameDiv.insertBefore(hourlyNameIcon, nameOutputDiv);
+			}
+		}
+
+		getActiveInputs();
+		validateInputArray();
+		convertInputToFloat();
+		calculateTotals();
+		calculateAllPerHours();
+		displayAllData();
+		appState = false;
+		console.table(allData);
+	}
 })
 
 
